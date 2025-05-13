e58b1531e886acbd2e3238228bef17d84853d144
Wlisses <wlissespinheiro@hotmail.com>
2025-05-12 22:08:16 -0300

    Removendo o youtube

--- Home/urls.py
+++ Home/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # URL principal do app Home
-    path('api/youtube/', views.youtube_videos, name='youtube_videos'),
+    # path('api/youtube/', views.youtube_videos, name='youtube_videos'),
     path('boletim/', views.video_boletim, name='unique_video'),
 
 ]
